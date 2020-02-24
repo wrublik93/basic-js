5ae4dd3e849563155df46f564347a54957aa6b15
wrublik93 <wrublik.93@mail.ru>
2020-02-25 02:13:57 +0300

    feat: implement the function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,49 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let discardNext = '--discard-next';
+    let discardPrev = '--discard-prev';
+    let doubleNext = '--double-next';
+    let doublePrev = '--double-prev';
+    let resultArray = [];
+    if(!Array.isArray(arr)){
+        throw new Error();
+    };
+    if(Array.isArray(arr) && arr.length == 0){
+        return [];
+    };
+    for (let i = 0; i < arr.length; i++){
+        switch(arr[i]) {
+            case discardNext: {
+                if(i + 1 < arr.length){
+                    i++;
+                };
+                break;
+            }
+
+            case discardPrev: {
+                if(i - 1 >= 0){
+                    resultArray.pop();
+                };
+                break;
+            }
+
+            case doubleNext: {
+                if(i + 1 < arr.length){
+                    resultArray.push(arr[i + 1]);
+                };
+                break;
+            }
+
+            case doublePrev: {
+                if(i - 1 >= 0){
+                    resultArray.push(arr[i - 1]);
+                };
+                break;
+            }
+
+            default: {
+                resultArray.push(arr[i]);   
+            }
+        }
+    };
+    return resultArray;
 };
