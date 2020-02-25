19372a64666c74bfd98550753232ca806cf60c72
wrublik93 <wrublik.93@mail.ru>
2020-02-25 22:19:59 +0300

    feat: implement the class DepthCalculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,13 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, number = 1) {
+        let filteredArray = arr.filter(item => Array.isArray(item));
+        if (filteredArray.length == 0) {
+            return number;
+        } else {
+            let finishArray = filteredArray.reduce(function(a, b){
+                return a.concat(b);
+            })
+            return number + this.calculateDepth(finishArray);
+        }
     }
 };
