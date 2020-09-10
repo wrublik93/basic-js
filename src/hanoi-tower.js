cbcb65f37f6f22db791dbd485d6800ac3c3c226f
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-09-10 12:13:20 +0300

    fix: change test with new task and implement new solution for hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,10 +1,11 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turnsTotal = Math.pow(2, disksNumber) - 1;   //minimum number of turns
     let turnsSpeedSeconds = turnsSpeed / 3600;   //turns per second
-    let timeTotal = turnsTotal / turnsSpeedSeconds;  //time to solve
+    let timeTotal = Math.floor(turnsTotal / turnsSpeedSeconds);  //time to solve
     let object = {
         turns: turnsTotal,
         seconds: timeTotal
     };
+    console.log(object);
     return object;
 }
